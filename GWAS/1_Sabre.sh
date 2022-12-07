7466e2eb7ab66c8c8f8bb84028e3f58d187f83a6
coulombevin <85315923+coulombevin@users.noreply.github.com>
2022-12-07 09:58:05 -0500

    Update 1_Sabre.sh

--- GWAS/1_Sabre.sh
+++ GWAS/1_Sabre.sh
@@ -1,8 +1,8 @@
 module add StdEnv/2020 intel/2020.1.217 sabre/1.00
 
 #set variable
-DATA=/lustre07/scratch/vicou67/BVG-7003/Cours_9/GBS_data/FC20150701_1.fq
-BARCODE=/lustre07/scratch/vicou67/BVG-7003/Cours_9/GBS_data/FC20150701_1.txt
+DATA=/home/vicou67/scratch/BVG-7003/Cours_9/GBS_data/FC20150701_1.fq
+BARCODE=/home/vicou67/scratch/BVG-7003/Cours_9/GBS_data/FC20150701_1.txt
 
 #output a log of my sh
 exec &> sabre.log
