de74e3b77e833bbd783a1a111ef5eaaa856cde93
coulombevin <85315923+coulombevin@users.noreply.github.com>
2022-12-07 09:56:25 -0500

    Create 1_Sabre.sh

---
+++ GWAS/1_Sabre.sh
@@ -0,0 +1,11 @@
+module add StdEnv/2020 intel/2020.1.217 sabre/1.00
+
+#set variable
+DATA=/lustre07/scratch/vicou67/BVG-7003/Cours_9/GBS_data/FC20150701_1.fq
+BARCODE=/lustre07/scratch/vicou67/BVG-7003/Cours_9/GBS_data/FC20150701_1.txt
+
+#output a log of my sh
+exec &> sabre.log
+
+#execute sabre tool with variable to demultiplex and trim fastq
+sabre se -f $DATA -b $BARCODE -u FC20150701_1_Sabre.fastq
